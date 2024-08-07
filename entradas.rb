3b6d184415d111b15e3fa664bd9633e3319d4663
Coriolando <coriolando.medeiros@gmail.com>
2024-08-07 04:41:28 -0300

    Aprendendo sobre entradas

---
+++ entradas.rb
@@ -0,0 +1,9 @@
+print "Nome: "
+nome = gets.chomp
+
+print "Idade: "
+idade = gets.chomp.to_i
+print "Cidade: "
+cidade = gets.chomp
+
+puts "Eu sou #{nome}, tenho #{idade} anos e moro em #{cidade}"
