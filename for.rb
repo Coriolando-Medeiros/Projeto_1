e7c748b3ba8e83ee8fe5bd493ad9e1049d9c22e8
Coriolando <coriolando.medeiros@gmail.com>
2024-08-08 05:15:20 -0300

    Utilizando FOR

---
+++ for.rb
@@ -0,0 +1,10 @@
+#repetição de um comando em um código
+#For (durante) faça
+linguagens = ["Ruby", "Python", "Javascript", "Java", "C", "C++" ]
+count = 1
+
+for linguagem in linguagens
+  puts linguagem
+  puts count
+  count = count + 1
+end
