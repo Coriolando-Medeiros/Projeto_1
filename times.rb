3e60299b979dedcfec4798db4dcdbe2270472322
Coriolando <coriolando.medeiros@gmail.com>
2024-08-08 05:28:59 -0300

    Trabalhando com Times

---
+++ times.rb
@@ -0,0 +1,16 @@
+#Repete uma ação um determinado número de vezes
+
+#Exemplo com uma linha
+6.times {puts "Oi"}
+
+#Exemplo com mais de uma linha
+4.times do
+  puts "Oi"
+  puts "Tudo bem?"
+end
+
+
+#exemplo de contador ate 20
+21.times do |contador|
+  puts "#{contador}"
+end
