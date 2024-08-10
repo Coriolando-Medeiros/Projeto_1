608e8870e83cca182124f8d868688aa222bdd8e0
Coriolando <coriolando.medeiros@gmail.com>
2024-08-10 09:08:19 -0300

    Abstração e encapsulamento

---
+++ POO/classes.rb
@@ -0,0 +1,19 @@
+#Classes
+#Definindo classes
+
+class Televisao
+
+  def turn_on
+    "Televisão ligada"
+  end
+
+  def shutdown
+    "Televisão desligada"
+  end
+  
+end
+
+televisao = Televisao.new
+
+puts televisao.turn_on
+puts televisao.shutdown
