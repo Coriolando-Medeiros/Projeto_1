a48da36fef4430206772f8279b4b53e1322997c2
Coriolando <coriolando.medeiros@gmail.com>
2024-08-14 05:11:42 -0300

    Aprendendo Modules

---
+++ Modulo_5/modules.rb
@@ -0,0 +1,19 @@
+#serve como um container para agrupar objetos relacionados
+
+
+module PalavraReversa
+  def self.puts text
+    print text.reverse.to_s
+  end
+end
+
+module PalavraNormal
+  def self.puts text
+    print text
+  end
+end
+
+PalavraReversa::puts 'O resultado é'
+puts ' '
+PalavraNormal::puts 'Palavra normal'
+puts ' '
